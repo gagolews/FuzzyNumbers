c281270349e8fb2d8dd3f4fc03e891574337ab77
gagolews <egzaltowany@gmail.com>
2013-05-29 16:12:57 +0200

    vignette_prepare.sh

--- src/vignette-Tutorial/pdfcompress.sh
+++
@@ -1,3 +0,0 @@
-#!/bin/bash
-gs -dNOPAUSE -dBATCH -sDEVICE=pdfwrite -dCompatibilityLevel=1.4 \
-   -dPDFSETTINGS=/ebook -sOutputFile=$2 $1
---
+++ src/vignette-Tutorial/vignette_prepare.sh
@@ -0,0 +1,3 @@
+#!/bin/bash
+gs -dNOPAUSE -dBATCH -sDEVICE=pdfwrite -dCompatibilityLevel=1.4 \
+   -dPDFSETTINGS=/ebook -sOutputFile=../../inst/doc/FuzzyNumbers-Tutorial.pdf FuzzyNumbers-Tutorial.pdf
